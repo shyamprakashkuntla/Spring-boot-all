4229f33ab1f34fa9b52affd67717f300c9b096b0
shyam prakash kuntla <shyamprakash.kuntla@gmail.com>
2025-05-31 19:42:16 +0530

    edited now

--- springdatajpawithuploadanddownload/src/main/java/com/sam/springdatajpawithuploadanddownload/exceptionhandlers/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.sam.springdatajpawithuploadanddownload.exceptionhandlers;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String msg)
-    {
-        super(msg);
-    }
-}
---
+++ src/main/java/com/sam/springdatajpawithuploadanddownload/constants/CommonConstants.java
@@ -0,0 +1,11 @@
+package com.sam.springdatajpawithuploadanddownload.constants;
+
+public class CommonConstants {
+
+    private CommonConstants() {
+    }
+
+    public static final String SUCCESS = "SUCCESS";
+
+    public static final String FAILED = "FAILED";
+}
